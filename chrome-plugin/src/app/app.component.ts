d9dfc0782c1423a4e47688f304d73278d967b281
Innov8 <Innov8@I8MacPro.local>
2017-07-08 21:08:15 -0700

    [mule-chorme-plugin] working angular page, blocked with background support

--- chrome-plugin/src/app/app.component.ts
+++ chrome-plugin/src/app/app.component.ts
@@ -1,21 +1,43 @@
 /// <reference types="chrome" />
 
 import { Component } from '@angular/core';
+import { OnInit} from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Mule';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor( private heroService: HeroService) { }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   interceptClick() {
     console.log( 'Clicked Intercept');
     chrome.webRequest.onBeforeRequest.addListener(this.requestProcessor);
   }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  getAsyncHeroes(): void {
+    this.heroService.getHeroesAsync().then(asyncHeroes => this.heroes = asyncHeroes);
+  }
+
 
   requestProcessor(details) {
     console.log(details);
+  }
+  onSelectHero(hero) {
+    this.selectedHero = hero;
   }
 }
